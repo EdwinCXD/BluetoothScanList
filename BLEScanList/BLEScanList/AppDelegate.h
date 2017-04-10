81309679732ab3f225566a7215a9627a62865c5e
EdwinChen <edwinchen2333@gmail.com>
2017-04-10 09:27:20 +0800

    first init

---
+++ BLEScanList/BLEScanList/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  BLEScanList
+//
+//  Created by EdwinChen on 2017/4/10.
+//  Copyright © 2017年 EdwinChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
