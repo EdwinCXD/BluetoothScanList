23bd8bcf68e261271a83cbda352184189b10f360
EdwinChen <edwinchen2333@gmail.com>
2017-04-10 09:53:50 +0800

    新建蓝牙数据模型

---
+++ BLEScanList/BLEScanList/Classes/Models/EDBluetoothModel.h
@@ -0,0 +1,17 @@
+//
+//  EDBluetoothModel.h
+//  BLEScanList
+//
+//  Created by EdwinChen on 2017/4/10.
+//  Copyright © 2017年 EdwinChen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+@interface EDBluetoothModel : NSObject
+
+@property (nonatomic, strong) CBPeripheral *peripheral;
+@property (nonatomic, strong) NSNumber *RSSI;
+@property (nonatomic, copy) NSString *macAddress;
+@end
---
+++ BLEScanList/BLEScanList/Classes/Models/EDBluetoothModel.m
@@ -0,0 +1,13 @@
+//
+//  EDBluetoothModel.m
+//  BLEScanList
+//
+//  Created by EdwinChen on 2017/4/10.
+//  Copyright © 2017年 EdwinChen. All rights reserved.
+//
+
+#import "EDBluetoothModel.h"
+
+@implementation EDBluetoothModel
+
+@end
